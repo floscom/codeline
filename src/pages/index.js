15589c3038c0d285ed6509efa3e8d9e2aaa7251f
Flo Narr <florian.narr@mail.com>
2017-11-02 23:33:12 +0100

    added some style

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,8 @@ import _more from '../assets/more-100px.png';
 
 import Skill from '../Components/Skill';
 
+import TextField from 'material-ui/TextField';
+
 // import {SectionsContainer, Section, ScrollToTopOnMount} from 'react-fullpage';
 
 /*
@@ -48,7 +50,7 @@ const IndexPage = ({ data }) => {
         </div>
 
         <div className="flexView hero">
-            <div className="mainFlex rowFlex">
+            <div className="hidden-xs mainFlex rowFlex">
                 <ul>
                     <li>Develop</li>
                     <li>Distribute</li>
@@ -62,15 +64,27 @@ const IndexPage = ({ data }) => {
                 <div><b>Software.</b> This is where I am an</div>
                 <div className="icon"><img src={expert} /></div>
             </div>
+            <div className="visible-xs text-center">
+                <ul>
+                    <li>Develop</li>
+                    <li>Distribute</li>
+                    <li>Monitor</li>
+                    <li>Extend</li>
+                    <li>Modernize</li>
+                    <li>Secure</li>
+                    <li>Manage</li>
+                </ul>
+                <img src={bracket} className="rotate90deg" />
+                <div><b>Software.</b><br />This is where I am an <img src={expert} /></div>
+            </div>
         </div>
 
         <div className="container broad hero">
             <div className="row">
-                <div className="col-sm-6 text-big">
-                    Entscheidend<br/>für deinen <b>Erfolg</b><br />sind deine <b>Handlungen</b>!
+                <div className="col-sm-6 text-big bb">
+                    Entscheidend<br/>für deinen <span className="hero-text">Erfolg</span><br />sind deine <span className="hero-text">Handlungen</span>!
                 </div>
-                <div className="col-sm-6">
-                    <hr className="visible-xs" />
+                <div className="col-sm-6 bl">
                     Ansprechendes Design ist schön und gut. Für gelungene Kommunikation braucht es aber mehr. Als Multi-Channel-Werbeagentur mit mehr als 15 Jahren Erfahrung verbinden wir unseren Anspruch an gehobenes Design mit fundiertem Know-how für alle Kanäle und bieten Ihnen Online- und Offline Marketing-Konzepte, die alles abdecken, was Werbung zu bieten hat. Von der Kreation bis hin zur Positionierung Ihrer Marke.
                 </div>
             </div>
@@ -100,6 +114,14 @@ const IndexPage = ({ data }) => {
                     })}
                 </div>
             </div>
+        </div>
+
+        <div className="container" style={{padding: "50px"}}>
+            <TextField
+                label="With placeholder"
+                placeholder="Placeholder"
+                margin="normal"
+            />
         </div>
 
     </div>
